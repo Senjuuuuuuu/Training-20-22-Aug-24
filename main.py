626913cba6e99e7e5b9b0f984aeaef8e3e1a06e7
Senjuuuuuuu <6710025003@psu.ac.th>
2024-08-21 14:52:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 
 df = sns.load_dataset('iris')
@@ -16,7 +19,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 
 st.sidebar.title('Classifiers')
-classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM','Decision Tree', 'Random Forest', 'Neural Network'))
 
 if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
@@ -31,3 +34,21 @@ if classifier == 'SVM':
   y_pred = svm.predict(x_test)
   accuracy_score(y_test, y_pred)
   st.write(acc)
+
+if classifier == 'Decision Tree':
+  dt = DecisionTreeClassifier()
+  dt.fit(x_train, y_train)
+  y_pred = dt.predict(x_test)
+  accuracy_score(y_test, y_pred)
+
+if classifier == 'Random Forest':
+  rf = RandomForestClassifier()
+  rf.fit(x_train, y_train)
+  y_pred = rf.predict(x_test)
+  accuracy_score(y_test, y_pred)
+
+if classifier == 'Neural Network':
+  nn = MLPClassifier()
+  nn.fit(x_train, y_train
+  y_pred = nn.predict(x_test)
+  accuracy_score(y_test, y_pred)
